162e04728a23008738fcb22adf5d049650ff7508
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-10 14:35:11 +0900

    [start]4-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,11 @@
 class BlogsController < ApplicationController
-  def index
-  end
 
   def show
+  	@blog = Blog.find(params[:id])
+  end
+
+  def index
+  	@blogs = Blog.all
   end
 
   def new
@@ -12,7 +15,7 @@ class BlogsController < ApplicationController
   def create
   	blog = Blog.new(blog_params)
     blog.save
-    redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit
@@ -20,6 +23,7 @@ class BlogsController < ApplicationController
 
   private
   def blog_params
-  params.require(:bolg).permit(:title, :category, :body)
+  	params.require(:blog).permit(:title, :category, :body)
   end
+
 end
