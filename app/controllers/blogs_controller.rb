90eafa69400b5aff49e83b7884e2220642a96fa8
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-10 14:07:34 +0900

    [Finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,12 +6,20 @@ class BlogsController < ApplicationController
   end
 
   def new
-  	@blog = blog.new
+  	@blog = Blog.new
   end
 
   def create
+  	blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blogs_path
   end
 
   def edit
+  end
+
+  private
+  def blog_params
+  params.require(:bolg).permit(:title, :category, :body)
   end
 end
